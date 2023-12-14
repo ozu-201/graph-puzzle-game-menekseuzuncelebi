7955603de15fcd1b29728e34ad37cca3311947b8
mu028519 <menekse.uzuncelebi@ozu.edu.tr>
2023-12-14 13:52:24 +0300

    lab1

---
+++ EdgeList.h
@@ -0,0 +1,26 @@
+//
+// Created by mu028519 on 12/14/2023.
+//
+
+#ifndef GRAPH_PUZZLE_GAME_MENEKSEUZUNCELEBI_EDGELIST_H
+#define GRAPH_PUZZLE_GAME_MENEKSEUZUNCELEBI_EDGELIST_H
+
+
+#include "Edge.h"
+
+class EdgeList {
+private:
+    Edge* head;
+    Edge* tail;
+public:
+    EdgeList();
+    ~EdgeList();
+    bool isEmpty() const;
+    Edge* search(int to) const;
+    void insert(Edge* newEdge);
+    Edge* getHead();
+};
+
+
+
+#endif //GRAPH_PUZZLE_GAME_MENEKSEUZUNCELEBI_EDGELIST_H
